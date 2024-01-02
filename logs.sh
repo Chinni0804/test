d5ea345d56f205b1b19d7286d337c048a2c588be
Chinni0804 <mydevops0804@gmail.com>
2024-01-02 12:53:48 +0530

    test1

---
+++ logs.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+ID=$(id-u)
+
+echo "$0"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then 
+        echo "ERROR:: $2 .... Myql fail"
+    else
+        echo "Install $2 ...  sucess"
+    fi
+
+}
+
+if [$ID -ne 0]
+then
+    echo " ERROR:: Please run as root user"
+    exit 1
+else
+    echo " you are a root user"
+fi
+
+yum install mysql -y
+
+VALIDATE $? "Mysql Success"
+
+yum install git -y
+
+VALIDATE $? "Installing git"
