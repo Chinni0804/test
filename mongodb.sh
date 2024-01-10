935603950ed4906a5c6f6e34f46496a57b5f20f9
Chinni0804 <mydevops0804@gmail.com>
2024-01-10 16:53:05 +0530

    robo shell

---
+++ mongodb.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "Script started executing at $TIMESTAMP" &>> $LOGFILE
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 .. $R Failed $N"
+    else
+        echo -e "$2 .. $G Success $N"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then
+    echo -e "$R ERROR:: Please run as root access $N"
+    exit 1 # We can give otherthan o
+else
+    echo "You are root user"
+fi # fi means reverse of if indication conditon end
+
+cp mongo.repo /etc/yum.repos.d
