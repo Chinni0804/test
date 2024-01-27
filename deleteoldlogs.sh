45dbc5ddb447ed2211e4d6d9340842982c5519a3
Chinni0804 <mydevops0804@gmail.com>
2024-01-27 11:17:34 +0530

    test

---
+++ deleteoldlogs.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+SOURCE_DIR=/tmp/shellscript-logs
+
+if [ ! -d $SOURCE_DIR ] # -d not exits directory
+then    
+    echo -e "$R Source directory: $SOURCE_DIR not exits $N"
+fi
+
+
+FILES_TO_DELETE=$(find . -type f -mtime +14 -name "*.log")
+
+while IFS= read -r line
+do
+    echo "Deleting file: $line"
+
+done <<< $FILES_TO_DELETE
+
+
